35b92f43867bcbfd6aef46f7d189325b263e3f08
nakao <yukidaruma_rinrin@hotmail.com>
2015-01-20 14:54:52 +0900

    Initialize repository

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,6 +20,13 @@ describe "Static pages" do
 			expect(page).to have_content('Sample App')
 		end
 		
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			#expect(page).to have_title("Home")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			#Š®‘Sˆê’v‚·‚é•¶š—ñ‚Å‚Í‚È‚­A•”•ª•¶š—ñ‚ğw’è‚·‚é‚¾‚¯‚Å‚à‚æ‚¢
+		end
+		
 	end
 		
 	describe "Help page" do
@@ -28,7 +35,12 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
-	
+		
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Help")
+		end	
+		
 	end
 	
 	describe "About page" do
@@ -38,6 +50,11 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
 			
+		end
+		
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("About Us")
 		end
 		
 	end
